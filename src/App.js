e7fe437e818a945fac54cd775e79cfead23585f8
20010308 <xolmuminovumidjon0@gmail.com>
2021-10-23 10:07:18 +0500

    interactive service

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import HomePageSecion1 from "./components/HomePageSecion1";
 import Home from "./components/Home";
 import Gealogiya from "./components/Gealogiya";
 import Enterprise1 from "./components/About the enterprise/Enterprise1";
+import ReportingSpecialists from "./components/interactive_service/ReportingSpecialists";
+import ReceiptOfReports from "./components/interactive_service/ReceiptOfReports";
+import Cadastre from "./components/interactive_service/Cadastre";
 
 const App = () => {
     return (
@@ -18,6 +21,12 @@ const App = () => {
             <Switch>
                 <Route path="/" exact component={Gealogiya}/>
                 <Route path="/enterprise1" exact component={Enterprise1}/>
+
+
+                /************         UMIDJON          ***************/
+                <Route path="/specialists" exact component={ReportingSpecialists}/>
+                <Route path="/reports" exact component={ReceiptOfReports}/>
+                <Route path="/cadastre" exact component={Cadastre}/>
             </Switch>
         </BrowserRouter>
     );
